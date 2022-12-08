3390ed623a51935be7e48fec5cbca1434ec2ae47
SENAN111 <behbudovsenan0@gmail.com>
2022-12-09 00:06:00 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string>list=new List<string>();
+            add(list);
+            
+          foreach(string s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static void add(List<string> list )
+        {
+            string name =Console.ReadLine();
+            list.Add(name);
+        }
+    }
+}
